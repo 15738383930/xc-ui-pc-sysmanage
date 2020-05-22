034fcd957fc37e164308f2f0ddddcef5519e84c0
zhouhao <15738383930@163.com>
2020-05-22 16:47:05 +0800

    增加了TestVue新增用户功能。

--- src/module/test/api/cms.js
+++ src/module/test/api/cms.js
@@ -5,3 +5,7 @@ export const page_list = (page, size, param) => {
   let stringify = querystring.stringify(param);
   return http.requestQuickGet('/test/vue/user/list/'+ page +'/' + size + '?' + stringify);
 };
+
+export const user_add = (user) => {
+  return http.requestPost('/test/vue/user/add', user);
+};
--- src/module/test/page/page_add.vue
+++ src/module/test/page/page_add.vue
@@ -1,9 +1,92 @@
 <template>
   <div>
+    <el-form :model="userForm" status-icon :rules="rules" ref="userForm" label-width="100px" class="demo-userForm">
+    <el-form-item label="人物名称" prop="name">
+      <el-input type="text" v-model="userForm.name" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="年龄" prop="age">
+      <el-input v-model.number="userForm.age"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm()">提交</el-button>
+      <el-button @click="resetForm('userForm')">重置</el-button>
+    </el-form-item>
+  </el-form>
   </div>
 </template>
-<script>
-</script>
-<style>
+    <script>
+      import * as userApi from '../api/cms';
+      export default {
+        data() {
+          let checkAge = (rule, value, callback) => {
+            if (!value) {
+              return callback(new Error('年龄不能为空'));
+            }
+            setTimeout(() => {
+              if (!Number.isInteger(value)) {
+                callback(new Error('请输入数字值'));
+              } else {
+                if (value < 18) {
+                  callback(new Error('必须年满18岁'));
+                } else {
+                  callback();
+                }
+              }
+            }, 1000);
+          };
+          let checkName = (rule, value, callback) => {
+            if (value === '') {
+              callback(new Error('请输入人物名称'));
+            } else {
+              callback();
+            }
+          };
+          return {
+            userForm: {
+              name: '',
+              age: ''
+            },
+            rules: {
+              name: [
+                { validator: checkName, trigger: 'blur' }
+              ],
+              age: [
+                { validator: checkAge, trigger: 'blur' }
+              ]
+            }
+          };
+        },
+        methods: {
+          submitForm() {
+            this.$refs.userForm.validate((valid) => {
+              if (valid) {
+                this.$confirm('确认提交吗？', '提示', {}).then(() => {
+                  this.addLoading = true;
+                  userApi.user_add(this.userForm).then((res) => {
+                    if(res.success){
+                      this.addLoading = false;
+                      this.$message({
+                        message: '提交成功',
+                        type: 'success'
+                      });
+                      this.$refs['userForm'].resetFields();
 
-</style>
+                    }else if(res.message){
+                      this.addLoading = false;
+                      this.$message.error(res.message);
+                    }else{
+                      this.addLoading = false;
+                      this.$message.error('提交失败');
+                    }
+                  });
+                });
+              }
+            });
+
+          },
+          resetForm(formName) {
+            this.$refs[formName].resetFields();
+          }
+        }
+      }
+    </script>
